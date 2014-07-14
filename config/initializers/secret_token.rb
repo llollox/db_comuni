3176ac74db796971cd03f29b6c7b996c64cd92d3
Lorenzo Rigato <lore91tanz@gmail.com>
2014-07-14 09:25:35 +0200

    added secret token to repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComuniItaliani::Application.config.secret_token = 'ed7aa058b98fbb91ad0ba655644473d4588fbf3ff2ab5095ae087a09fa4418a2fb5438cd3b36fa3292b1398b81de4eb5cce12e418f09613d4c016db4ff973425'
